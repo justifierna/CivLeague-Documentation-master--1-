7346acc03aafb3ca9272a1309722cfa7714ac757
JustifiernNA <justifierna@gmail.com>
2017-04-05 20:35:58 -0500

    Game 133

---
+++ CivLeague-Documentation-master/Evidence/Asura Climaximum Issue.sql
@@ -0,0 +1,390 @@
+"""Asura Says""" were all in
+  """Asura Says""" were all in
+"""Asura Says""" ready up
+  """Asura Says""" ready up
+
+  """Climaximum Says""" what
+  """Climaximum Says""" im ready
+  """Climaximum Says""" scremin at me
+  """Climaximum Says""" press q
+
+"""Asura Says""" wanna be friend?
+"""Asura Says""" i wont betray you
+"""Asura Says""" but dont settle more lands to the east
+"""Asura Says""" its a peninsula
+"""Asura Says""" i think im locked from the others
+"""Asura Says""" youre my wall
+"""Asura Says""" it means i dont have acces to others
+"""Asura Says""" btw
+"""Asura Says"""  horse barbs incoming from ur southeast
+"""Asura Says""" they are raping my warrior
+"""Asura Says""" i let you build  more city to the east
+"""Asura Says""" pin
+"""Asura Says""" team up and beat him
+"""Asura Says""" whos aztec
+"""Asura Says""" hes getting big
+"""Asura Says""" better do something asap
+
+"""Asura Says""" too bad im too far from everyone
+
+"""Climaximum Says""" it has
+"""Climaximum Says""" and toronro is mine
+"""Climaximum Says""" Peace with me?
+
+"""Asura Says""" encampment and wall looks like youre the one going to betray me
+"""Asura Says""" k
+"""Asura Says""" err i dont have much
+"""Asura Says""" i can archers
+"""Asura Says""" the other ones are too busy with barbs
+"""Asura Says""" ok
+"""Asura Says""" im making more archers
+
+"""Climaximum Says""" lol
+"""Climaximum Says""" A pact was made
+"""Climaximum Says""" schedules changed
+
+"""Asura Says""" start making lots of units
+"""Asura Says""" i dont have luxuries
+"""Asura Says""" share pls
+"""Asura Says""" prod is slowed
+"""Asura Says""" im making archers to help u
+"""Asura Says""" thx a lot
+
+"""Climaximum Says""" i got that pantanal locked down
+
+"""Asura Says""" tell russia that he better team up against aztec rather than attack u
+"""Asura Says""" how many cities aztec has?
+"""Asura Says""" told u to do something
+
+"""Climaximum Says""" well hes not movin his army from my border
+
+"""Climaximum Says""" I got  turns
+
+"""Asura Says""" i dont understand why people just let someone steamroll
+
+"""Asura Says""" when they could do something about it
+
+"""Climaximum Says""" s people were much r than i
+
+"""Asura Says""" does he have crossbows?
+"""Asura Says""" open ur border and tell me who he is
+"""Asura Says""" im ing archers
+
+"""Climaximum Says""" aint gettin through nowhere bud
+
+"""Asura Says""" whats his military score
+"""Asura Says""" you thinck or something?
+"""Asura Says""" unlike u im tryying to do something
+
+"""Climaximum Says""" your archers are puny you cant win
+"""Climaximum Says""" i did do something
+
+"""Asura Says""" still better than sitting and watching like a damn cow
+
+"""Climaximum Says""" i made peace with him so he doesnt kill me and kills others
+
+"""Asura Says""" he will kill you later
+"""Asura Says""" idiot
+
+"""Climaximum Says""" yeah after you guys
+
+"""Asura Says""" what a pussy you are
+
+"""Climaximum Says""" not my problem right now.
+"""Climaximum Says""" do something about it 
+"""Climaximum Says""" ohh wait you cant cause you losin P
+
+"""Asura Says""" whoa clim is a pussy
+"""Asura Says""" he wants aztec to carry him to nd place
+"""Asura Says""" after aztec attack us all
+"""Asura Says""" pathetic
+
+"""Climaximum Says""" how is that pathetic my army tri fold yours
+"""Climaximum Says""" his quad fold yours
+"""Climaximum Says""" sounds like powers of the world killin noobs
+
+"""Asura Says""" yes
+"""Asura Says""" with alliance
+"""Asura Says""" youre lucky i have shit land
+
+"""Climaximum Says""" just sayin aztec aint my only border to worry about
+"""Climaximum Says""" nah im lucky you shit city builder
+
+"""Asura Says""" clim
+"""Asura Says""" ur supposed to be moderator
+"""Asura Says""" watch your attitude
+"""Asura Says""" youre acting like a kid
+
+"""Climaximum Says""" well stop complaining to me about another player
+
+"""Climaximum Says""" thats childish play the game and not toalk and this wouldnt be a convo
+
+"""Asura Says""" hurrr durr your losing puny army i have threefold urs hurr durr
+
+"""Asura Says""" it isnt available now
+
+"""Climaximum Says""" mhmm
+
+"""Asura Says""" doesnt that sound pathetic?
+
+"""Climaximum Says""" noob 
+
+"""Asura Says""" guys can we kick clim for toxic behaviour?
+
+"""Climaximum Says""" sounds like you need to get a bigger army and stop complaining about mine
+
+"""Asura Says""" no one yet
+
+"""Climaximum Says""" no logic in making peace with aztec so his army dont stomp me
+"""Climaximum Says""" and killing you guys to kill him?
+"""Climaximum Says""" like learn to fucking play bro
+
+  """Asura Says""" whats up with this guy
+
+"""Climaximum Says""" just cause i dont wanna tell you my strategy you get butthurt
+
+"""Asura Says""" he talks like he knows better than everyone
+"""Asura Says""" we all know your strategy
+
+"""Climaximum Says""" weird how im only angry at nubs
+
+"""Asura Says""" youre begging ztec to let you live and get yourself nd place
+"""Asura Says""" youre begging ztec to let you live and get yourself nd place
+
+"""Climaximum Says""" No.
+"""Climaximum Says""" i made peace with him
+"""Climaximum Says""" to kill poland
+"""Climaximum Says""" and you
+"""Climaximum Says""" so i got alnd 
+"""Climaximum Says""" to make cities
+"""Climaximum Says""" to kill aztec 
+
+"""Asura Says""" lets kill russia
+"""Asura Says""" but need better units
+"""Asura Says""" i asked him to open border so i could  my army to help against aztec
+"""Asura Says""" the dude say shit like aint nobiody passing thru me bud
+"""Asura Says""" open border and tell me wheres the fight im ing army
+"""Asura Says""" sure how much
+"""Asura Says""" sent
+"""Asura Says"""  turns
+"""Asura Says""" accept my money no?
+"""Asura Says""" accept delegation
+"""Asura Says""" u get money from delegation
+"""Asura Says""" and  me one after
+"""Asura Says"""  me delegation so iget money too
+"""Asura Says""" accept delegation its free money for u
+"""Asura Says""" you shouldnt have
+"""Asura Says""" nvm
+"""Asura Says""" wroong chat
+"""Asura Says"""  me one too pls
+"""Asura Says""" want help against russia?
+"""Asura Says""" i dont but when ill get my own religion ill erase yours from m cities
+"""Asura Says""" were attackign russia right?
+"""Asura Says""" i wont get my religion anytime soon
+"""Asura Says""" im waiting for my last prophet ability
+"""Asura Says""" how far are u from civil service
+"""Asura Says""" it allows alliance
+"""Asura Says""" dont mind that
+"""Asura Says""" i have a few allies in this game
+"""Asura Says""" how far are u from civil service
+"""Asura Says""" i need alliance to upgrade my units in your territory
+"""Asura Says""" kk
+"""Asura Says""" trying
+"""Asura Says""" lets attack him next turn? he is busy with poland
+"""Asura Says""" how many turns do you need
+"""Asura Says""" no
+"""Asura Says""" just tell me when you are ready
+"""Asura Says""" ok
+"""Asura Says""" lets wait  turns
+"""Asura Says""" well
+"""Asura Says""" i just remembered i still need mercenaries to reduce price of upgrade
+"""Asura Says""" so i thought better start sooner and upgrade some later
+"""Asura Says""" you know what
+"""Asura Says""" just do mercenaries
+"""Asura Says""" i give u money to upgrade
+"""Asura Says""" ?
+"""Asura Says""" yeah...
+"""Asura Says""" yes
+
+"""Climaximum Says""" 
+
+"""Asura Says""" i just denounced
+
+"""Climaximum Says""" muffintops
+
+"""Asura Says""" lets ambush the units that are going to poland city
+
+"""Climaximum Says""" sumeria and arabia at my borders
+"""Climaximum Says""" easy to take them out when you get there
+"""Climaximum Says""" im pinned with poland on the other side
+"""Climaximum Says""" might be able to take em out before you come for me
+
+"""Asura Says""" ok
+
+"""Asura Says""" ill  u money
+"""Asura Says""" were coming to attack russia in  turns
+"""Asura Says""" ask gilga for spare luxes
+
+"""Climaximum Says""" lol fck off barbs
+
+"""Asura Says""" its ok
+"""Asura Says""" ill give u money next turn
+
+"""Climaximum Says""" So you can freely insult a moderator and i can you ?
+"""Climaximum Says""" sound fair?
+
+"""Asura Says""" insult a moderator
+"""Asura Says""" now that sounds like someone who boast about having special power
+"""Asura Says""" youre the first to call people noob
+"""Asura Says""" we were
+
+"""Climaximum Says""" nah just kids thinking they can just say whatever to whoever without a consequence or expecting a rthen complaining about said rsaying im supposed to be a moderator
+
+"""Asura Says""" but russia has other plans
+
+"""Climaximum Says""" what does that mean?
+"""Climaximum Says""" Position dont dictate behaviour child
+
+"""Asura Says""" no
+"""Asura Says""" i said youre supposed to be a moderator
+"""Asura Says""" cos you started to have a toxic attitude
+"""Asura Says""" next turn
+"""Asura Says""" declare now?
+"""Asura Says""" kill the horseman south of kishi? said *shrug* just have to get more experience at online dynamic I guess
+"""Asura Says""" next turn
+
+"""Climaximum Says""" lets go aztecs
+"""Climaximum Says""" carry me to nd
+
+"""Asura Says""" cheers luv the cavalrys here
+"""Asura Says""" ran out of move
+
+"""Asura Says""" what a joke calling people noob but asking for the runaway civ to carry him nd place
+
+"""Climaximum Says""" dont get mad you cant make friends said Ill admit hes somewhat annoying but continuing to spam chat with backandforth abuse isnt helpful
+
+"""Climaximum Says""" im just helping him win at this point
+
+"""Asura Says""" negi why are you helping that asshole?
+
+"""Asura Says""" you arent helping anything
+
+"""Asura Says""" he doesnt need you
+
+"""Climaximum Says""" lol im helping him thats the part you see assholish
+
+"""Asura Says""" you arent helping him
+"""Asura Says""" he doesnt need you to win
+"""Asura Says""" youre pathetic
+"""Asura Says""" asking for someone to carry you
+"""Asura Says""" to nd place
+
+"""Climaximum Says""" not as pathetic as rd
+"""Climaximum Says""" bdum tss
+
+"""Asura Says""" nobody is going to agree to put you nd place
+
+"""Climaximum Says""" i know that
+
+"""Climaximum Says""" i have peace in knowing he is gonna kill me
+
+"""Asura Says""" nobody will agree on you taking nd place
+
+"""Asura Says""" thats it
+
+"""Climaximum Says""" lol idgaf
+
+"""Asura Says""" hiding behind the big guy
+
+"""Climaximum Says""" i dont count my exp
+"""Climaximum Says""" i just play
+
+"""Asura Says""" looks like you do
+"""Asura Says""" since you asked him to carry you
+"""Asura Says""" you arent playing
+"""Asura Says""" you are being carried
+
+"""Climaximum Says""" ii traded him and made myself an ally
+
+"""Climaximum Says""" thats playin bro
+
+"""Asura Says""" gucci look at my screenshot
+
+"""Asura Says""" im too far from eeverybody
+"""Asura Says""" and on shit land
+"""Asura Says""" still i was building army
+"""Asura Says""" to get  to aztec
+"""Asura Says""" you cant find fault in me
+"""Asura Says""" you have  for knight? i can  u money
+"""Asura Says""" i had to answer to that ass
+"""Asura Says""" who
+"""Asura Says""" that was years ago
+"""Asura Says""" now they are  pop on average
+"""Asura Says""" sorry out of move after attacking a swordsman
+"""Asura Says""" doesnt matter
+"""Asura Says""" end game is all about who has the most cities
+"""Asura Says"""  me alliance
+"""Asura Says""" as of now
+"""Asura Says"""  aztec
+"""Asura Says""" nd tied sumeria russia arabia poland
+"""Asura Says""" th germany
+"""Asura Says""" vote for end
+
+"""Climaximum Says""" Im playin it out
+"""Climaximum Says""" meh i told you verbally
+
+"""Asura Says""" doesnt matter
+
+"""Asura Says""" the score i put
+
+"""Climaximum Says""" didint expect to win
+
+"""Asura Says""" will have majority vote
+
+"""Asura Says""" +
+"""Asura Says""" clim isnt allowed to vote
+
+"""Climaximum Says""" so im not playing?
+
+"""Asura Says""" youre playing but since you want aztec to carry you nd place
+
+"""Climaximum Says""" S asura dont be rude
+"""Climaximum Says""" thats playing 
+
+"""Asura Says""" its not allowed
+
+"""Climaximum Says""" play the game 
+"""Climaximum Says""" be mad
+
+"""Climaximum Says""" yeah it is
+
+"""Asura Says""" its not
+"""Climaximum Says""" diplo bruh
+
+"""Asura Says""" we vote no
+
+"""Climaximum Says""" Shoulda clarified it when you voted at the start of the game
+"""Climaximum Says""" its in now.
+
+"""Climaximum Says""" read the rules
+
+"""Asura Says""" its not allowed to do nothing and get carried
+
+"""Asura Says""" you have to actually matter
+
+"""Climaximum Says""" im fightin  people mang
+"""Climaximum Says""" 
+
+"""Asura Says""" his vote doesnt ocount
+
+"""Climaximum Says""" why should i 
+
+"""Asura Says""" he wants aztec to do all the job so he can get nd place
+"""Climaximum Says""" i got  settlers on the move
+
+"""Asura Says""" we all voted to end no? except the little guy who wants to get carried
+"""Asura Says""" who are the  who dont want to end?
+"""Asura Says""" we all voted to end
+
+"""Climaximum Says""" Little tab on the right makes chat box bigger 
+"""Climaximum Says""" then theres problems
